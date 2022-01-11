f3dbb049bfb2c81934ed8982a22299d6f2620d6c
milesreng <miles.eng@duke.edu>
2022-01-11 14:21:51 -0800

    completed dnamaxnucleotide apt

--- APT-2/DNAMaxNucleotide.java
+++ APT-2/DNAMaxNucleotide.java
@@ -1,7 +1,28 @@
 public class DNAMaxNucleotide {
 
     public String max(String[] strands, String nuc) {
-        return "";
+        String longest = strands[0];
+        int count = 0;
+
+        for (int i = 0; i < strands.length; i++) {
+            int newCount = 0;
+            for (int j = 0; j < strands[i].length(); j++) {
+                if (strands[i].charAt(j) == nuc.charAt(0)) {
+                    newCount++;
+                }
+            }
+            if (newCount > count) {
+                longest = strands[i];
+                count = newCount;
+            } else if (newCount == count && strands[i].length() > longest.length()) {
+                longest = strands[i];
+            }
+        }
+
+        if (count == 0) {
+            return "";
+        }
+        return longest;
     }
     
 }
