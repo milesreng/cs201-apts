372cbc882ff19afd57d7f824e1545da4780c6078
milesreng <miles.eng@duke.edu>
2022-01-11 14:03:02 -0800

    initial commit

--- APT-1/Totality.java
+++ APT-1/Totality.java
@@ -1,12 +1,13 @@
 public class Totality {
+
     public int sum(int[] a, String stype) {
         int total = 0;
         for (int i = 0; i < a.length; i++) {
-            if (stype == "odd" && i % 2 != 0) {
+            if (stype.equals("even") && i % 2 == 0) {
                 total += a[i];
-            } else if (stype == "even" && i % 2 == 0) {
+            } else if (stype.equals("odd") && i % 2 == 1) {
                 total += a[i];
-            } else if (stype == "all") {
+            } else if (stype.equals("all")) {
                 total += a[i];
             }
         }
