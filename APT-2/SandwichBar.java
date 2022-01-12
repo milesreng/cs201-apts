9f475a2cebd3422095ebec946457b93871b5a2de
milesreng <miles.eng@duke.edu>
2022-01-12 12:44:43 -0800

    began sandwichbar apt

--- APT-2/SandwichBar.java
+++ APT-2/SandwichBar.java
@@ -1,6 +1,14 @@
 public class SandwichBar {
     
     public int whichOrder(String[] available, String[] orders) {
+        for (int i = 0; i < orders.length; i++) {
+            String[] order = orders[i].split(" ");
+            for (int j = 0; j < order.length; j++) {
+                if (available.contains(order[j])) {
+                    return 0;
+                }
+            }
+        }
         return 0;
     }
 
