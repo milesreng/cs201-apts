ceeb41ba35170ad1e13ff37f6d79da5c803dddbc
milesreng <miles.eng@duke.edu>
2022-01-13 20:41:57 -0800

    first attempt for TxMsg.java with main() method

--- APT-2/SandwichBar.java
+++ APT-2/SandwichBar.java
@@ -4,9 +4,7 @@ public class SandwichBar {
         for (int i = 0; i < orders.length; i++) {
             String[] order = orders[i].split(" ");
             for (int j = 0; j < order.length; j++) {
-                if (available.contains(order[j])) {
-                    return 0;
-                }
+                
             }
         }
         return 0;
--- APT-2/TxMsg.java
+++ APT-2/TxMsg.java
@@ -1,7 +1,34 @@
 public class TxMsg {
 
-    public String getMessage(String original) {
-        return "";
+    public static String getMessage(String original) {
+        String[] wordList = original.split(" ");
+        int i;
+        String abbr;
+
+        for (i = 0; i < wordList.length; i++) {
+            abbr = "";
+            Boolean hasConsonant = false;
+
+            for (int j = 0; j < wordList[i].length(); j++) {
+                if ("aeiouAEIOU".indexOf(wordList[i].charAt(j)) == -1) {
+                    if (j != 0 && "aeiouAEIOU".indexOf(wordList[i].charAt(j - 1)) != -1) {
+                        abbr += wordList[i].charAt(j);
+                    } else if (j == 0) {
+                        abbr += wordList[i].charAt(j);
+                    }
+                    hasConsonant = true;
+                }
+            }
+            if (hasConsonant) {
+                wordList[i] = abbr;
+            }
+        }
+        return String.join(" ", wordList);
+    }
+
+    public static void main(String[] args) {
+        String str = "text message";
+        System.out.println(getMessage(str));
     }
     
 }
