2f4a608b6eb2c851212a8d78583833025f9bac21
milesreng <miles.eng@duke.edu>
2022-01-13 22:02:04 -0800

    first attempt for Common.java

--- APT-2/Common.java
+++ APT-2/Common.java
@@ -1,7 +1,18 @@
+import java.util.*;
+
 public class Common {
 
     public int count(String a, String b) {
-        return 0;
+        HashSet<String> common = new HashSet<>();
+        List<String> aList = Arrays.asList(a.split(""));
+        List<String> bList = Arrays.asList(b.split(""));
+
+        for (String s : aList) {
+            if (bList.contains(s)) {
+                common.add(s);
+            }
+        }
+        return common.size();
     }
     
 }
