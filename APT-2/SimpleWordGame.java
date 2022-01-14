4515dae5f65fc3fdda69d41ae352362d50909936
milesreng <miles.eng@duke.edu>
2022-01-13 21:53:59 -0800

    first attempt for SimpleWordGame.java

--- APT-2/SimpleWordGame.java
+++ APT-2/SimpleWordGame.java
@@ -1,7 +1,20 @@
+import java.io.*;
+import java.util.*;
+
 public class SimpleWordGame {
 
     public int points(String[] player, String[] dictionary) {
-        return 0;
+        HashSet<String> unique = new HashSet<>(Arrays.asList(player));
+        List<String> wordList = Arrays.asList(dictionary);
+        int total = 0;
+
+        for (String s : unique) {
+            if (wordList.contains(s)) {
+                total += s.length() * s.length();
+            }
+        }
+
+        return total;
     }
     
 }
