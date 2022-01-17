eacadab3cbaeed537b3655ff4c3d98f6cdf9f771
milesreng <miles.eng@duke.edu>
2022-01-17 15:45:40 -0500

    truespace, 51 out of 81 failing on largest outputs

--- APT-3/TrueSpace.java
+++ APT-3/TrueSpace.java
@@ -1,3 +1,24 @@
 public class TrueSpace {
+
+    public long calculateSpace(int[] sizes, int clusterSize) {
+        long clusters;
+
+        long totalClusters = 0;
+        for (int i = 0; i < sizes.length; i++) {
+            if (sizes[i] != 0) {
+                clusters = (sizes[i] / clusterSize);
+                if (sizes[i] % clusterSize != 0) {
+                    totalClusters += clusters + 1;
+                } else {
+                    totalClusters += clusters;
+                }
+            }
+        }
+
+        long total = totalClusters * clusterSize;
+
+        return total;
+
+    }
     
 }
