5e5af34d25939ceb7a2972fb87c1e162f22f9eb3
milesreng <miles.eng@duke.edu>
2022-01-17 16:05:25 -0500

    completed counterattack apt

--- APT-3/CounterAttack.java
+++ APT-3/CounterAttack.java
@@ -1,3 +1,16 @@
 public class CounterAttack {
+
+    public int[] analyze(String str, String[] words) {
+        String[] strList = str.split(" ");
+        int[] count = new int[words.length];
+        for (int i = 0; i < strList.length; i++) {
+            for (int j = 0; j < words.length; j++) {
+                if (words[j].equals(strList[i])) {
+                    count[j]++;
+                }
+            }
+        }
+        return count;
+    }
     
 }
