9477213117941f1dd5f8ff0ce5fad764255f3fe5
milesreng <miles.eng@duke.edu>
2022-01-20 13:34:38 -0500

    completed PrefixCode.java (fixed equals operator for strings)

--- APT-3/PrefixCode.java
+++ APT-3/PrefixCode.java
@@ -4,7 +4,7 @@ public class PrefixCode {
         for (int i = 0; i < words.length; i++) {
             for (int j = 0; j < words.length; j++) {
                 if (i != j && words[i].length() <= words[j].length()) {
-                    if (words[i] == words[j].substring(0, words[i].length())) {
+                    if (words[i].equals(words[j].substring(0, words[i].length()))) {
                         return "No, " + i;
                     }
                 }
