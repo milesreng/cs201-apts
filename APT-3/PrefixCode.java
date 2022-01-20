0b20e3ee2b67812263daf833f5b75baf15564c77
milesreng <miles.eng@duke.edu>
2022-01-20 13:32:15 -0500

    initial test for prefixcode, 24/67

--- APT-3/PrefixCode.java
+++ APT-3/PrefixCode.java
@@ -1,3 +1,16 @@
 public class PrefixCode {
     
+    public String isOne(String[] words) {
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j < words.length; j++) {
+                if (i != j && words[i].length() <= words[j].length()) {
+                    if (words[i] == words[j].substring(0, words[i].length())) {
+                        return "No, " + i;
+                    }
+                }
+            }
+        }
+        return "Yes";
+    }
+
 }
