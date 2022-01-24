cae0df5eaf3bfab33da0fdba59a7ea735cd5960c
milesreng <miles.eng@duke.edu>
2022-01-24 16:31:48 -0500

    made counterattack.java more efficient using .frequency method

--- APT-3/CounterAttack.java
+++ APT-3/CounterAttack.java
@@ -1,14 +1,12 @@
+import java.util.*;
+
 public class CounterAttack {
 
     public int[] analyze(String str, String[] words) {
-        String[] strList = str.split(" ");
+        List<String> strList = Arrays.asList(str.split(" "));
         int[] count = new int[words.length];
-        for (int i = 0; i < strList.length; i++) {
-            for (int j = 0; j < words.length; j++) {
-                if (words[j].equals(strList[i])) {
-                    count[j]++;
-                }
-            }
+        for (int i = 0; i < words.length; i++) {
+            count[i] = Collections.frequency(strList, words[i]);
         }
         return count;
     }
