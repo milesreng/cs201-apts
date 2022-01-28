c082353be654383300a56fcc1bb592c9c3e5877a
milesreng <miles.eng@duke.edu>
2022-01-28 14:28:27 -0500

    improved anonymous.java using ascii values

--- APT-4/Anonymous.java
+++ APT-4/Anonymous.java
@@ -2,38 +2,37 @@ import java.util.*;
 
 public class Anonymous {
     public int howMany(String[] headlines, String[] messages) {
-        ArrayList<Character> chars = new ArrayList<Character>();
-        
-        for (String headline : headlines) {
-            for (int i = 0; i < headline.length(); i++) {
-                if (headline.charAt(i) != ' ') {
-                    chars.add(Character.toLowerCase(headline.charAt(i)));
-                }
-            }
-        }
+        int[] hlCounts = getCounts(headlines);
 
         int count = 0;
         for (String message : messages) {
             Boolean hasChars = true;
-            ArrayList<Character> availChars = new ArrayList<Character>();
-            availChars.addAll(chars);
 
-            for (int i = 0; i < message.length(); i++) {
-                if (message.charAt(i) != ' ') {
-                    Character currChar = Character.toLowerCase(message.charAt(i));
-                    if (availChars.contains(currChar)) {
-                        availChars.remove(availChars.indexOf(currChar));
-                    } else {
-                        hasChars = false;
-                        break;
-                    }
+            String[] m = message.split(" ");
+            int[] currCounts = getCounts(m);
+
+            for (int i = 0; i < currCounts.length; i++) {
+                if (hlCounts[i] < currCounts[i]) {
+                    hasChars = false;
+                    break;
                 }
             }
+
             if (hasChars) {
                 count++;
             }
         }
 
         return count;
+    }
+
+    private int[] getCounts(String[] strings) {
+        int[] counts = new int[300];
+        for (String s : strings) {
+            for (char ch : s.toLowerCase().toCharArray()) {
+                counts[ch]++;
+            }
+        }
+        return counts;
     }
 }
