975d76114cc1cd2ef89a8c7dfd04b7160e849da5
milesreng <miles.eng@duke.edu>
2022-01-28 14:01:20 -0500

    more efficient method for membercheck.java

--- APT-4/MemberCheck.java
+++ APT-4/MemberCheck.java
@@ -2,28 +2,24 @@ import java.util.*;
 
 public class MemberCheck {
     public String[] whosDishonest(String[] club1, String[] club2, String[] club3) {
-        HashSet<String> set1 = new HashSet<String>(Arrays.asList(club1));
-        HashSet<String> set1intersect = new HashSet<String>(Arrays.asList(club1));
-        HashSet<String> set2 = new HashSet<String>(Arrays.asList(club2));
-        HashSet<String> set3 = new HashSet<String>(Arrays.asList(club3));
+        Set<String> set1 = new HashSet<String>(Arrays.asList(club1));
+        Set<String> set2 = new HashSet<String>(Arrays.asList(club2));
+        Set<String> set3 = new HashSet<String>(Arrays.asList(club3));
+        Set<String> common = new HashSet<String>();
 
-        HashSet<String> common = new HashSet<String>();
-        set1.retainAll(set2);
-        set1intersect.retainAll(set3);
-        set2.retainAll(set3);
+        common.addAll(intersection(set1, set2));
+        common.addAll(intersection(set1, set3));
+        common.addAll(intersection(set2, set3));
 
-        common.addAll(set1);
-        common.addAll(set1intersect);
-        common.addAll(set2);
-
-        String[] dish = new String[common.size()];
-        int i = 0;
-        for (String name : common) {
-            dish[i] = name;
-            i++;
-        }
+        String[] dish = common.toArray(new String[0]);
         Arrays.sort(dish);
 
         return dish;
+    }
+
+    private Set<String> intersection(Set<String> set1, Set<String> set2) {
+        Set<String> s = new HashSet<String>(set1);
+        s.retainAll(set2);
+        return s;
     }
 }
