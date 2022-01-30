7a78b16369ce06c1cef92433aa0bc7cfe260930d
milesreng <miles.eng@duke.edu>
2022-01-30 09:38:51 -0500

    completed pairdown.java

--- APT-5/PairDown.java
+++ APT-5/PairDown.java
@@ -1,5 +1,20 @@
+import java.util.*;
+
 public class PairDown {
     public int[] fold(int[] list) {
-        return new int[0];
+        ArrayList<Integer> combine = new ArrayList<Integer>();
+        for (int i = 0; i < list.length; i += 2) {
+            if (i == list.length - 1) {
+                combine.add(list[i]);
+            } else {
+                combine.add(list[i] + list[i + 1]);
+            }
+        }
+        int[] comb = new int[combine.size()];
+
+        for (int i = 0; i < comb.length; i++) {
+            comb[i] = combine.get(i);
+        }
+        return comb;
     }
 }
