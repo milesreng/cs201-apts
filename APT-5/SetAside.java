f1ee7f94a2111319067f16572a06ccc78733239a
milesreng <miles.eng@duke.edu>
2022-02-03 18:29:07 -0500

    reworked SetAside.java

--- APT-5/SetAside.java
+++ APT-5/SetAside.java
@@ -1,5 +1,31 @@
+import java.util.*;
+
 public class SetAside {
     public String common(String[] list) {
-        return list[0];
+        ArrayList<String> all = new ArrayList<String>();
+        for (int i = 0; i < list.length; i++) {
+            String[] words = list[i].split(" ");
+            for (int j = 0; j < words.length; j++) {
+                all.add(words[j]);
+            }
+        }
+
+        ArrayList<String> commonWords = new ArrayList<String>();
+        for (String word : all) {
+            if (Collections.frequency(all, word) == list.length) {
+                commonWords.add(word);
+            }
+        }
+
+        String[] ret = new String[commonWords.size()];
+
+        int i = 0;
+        for (String word : commonWords) {
+            ret[i] = word;
+            i++;
+        }
+        Arrays.sort(ret);
+
+        return String.join(" ", ret);
     }
 }
