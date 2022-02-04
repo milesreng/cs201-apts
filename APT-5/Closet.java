f972c8117d1069e8c405265eba642ead7e5b3ee5
milesreng <miles.eng@duke.edu>
2022-02-03 19:17:08 -0500

    completed Closet.java

--- APT-5/Closet.java
+++ APT-5/Closet.java
@@ -1,5 +1,21 @@
+import java.util.*;
+
 public class Closet {
     public String anywhere(String[] list) {
-        return list[0];
+        TreeSet<String> allWords = new TreeSet<String>();
+        for (String phrase : list) {
+            String[] words = phrase.split(" ");
+            for (int j = 0; j < words.length; j++) {
+                allWords.add(words[j]);
+            }
+        }
+
+        String[] last = new String[allWords.size()];
+        int i = 0;
+        for (String word : allWords) {
+            last[i] = word;
+            i++;
+        }
+        return String.join(" ", last);
     }
 }
--- APT-5/SetAside.java
+++ APT-5/SetAside.java
@@ -2,30 +2,28 @@ import java.util.*;
 
 public class SetAside {
     public String common(String[] list) {
-        ArrayList<String> all = new ArrayList<String>();
-        for (int i = 0; i < list.length; i++) {
-            String[] words = list[i].split(" ");
-            for (int j = 0; j < words.length; j++) {
-                all.add(words[j]);
+        HashMap<String, Integer> words = new HashMap<String, Integer>();
+        for (String phrase : list) {
+            String[] splitPhrase = phrase.split(" ");
+            for (String word : splitPhrase) {
+                words.putIfAbsent(word, 0);
+                words.put(word, words.get(word) + 1);
             }
         }
 
         ArrayList<String> commonWords = new ArrayList<String>();
-        for (String word : all) {
-            if (Collections.frequency(all, word) == list.length) {
-                commonWords.add(word);
+        for (String key : words.keySet()) {
+            if (words.get(key) == list.length) {
+                commonWords.add(key);
             }
         }
 
-        String[] ret = new String[commonWords.size()];
-
-        int i = 0;
-        for (String word : commonWords) {
-            ret[i] = word;
-            i++;
+        String[] w = new String[commonWords.size()];
+        for (int i = 0; i < commonWords.size(); i++) {
+            w[i] = commonWords.get(i);
         }
-        Arrays.sort(ret);
 
-        return String.join(" ", ret);
+        Arrays.sort(w);
+        return String.join(" ", w);
     }
 }
