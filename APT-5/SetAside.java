8275ca6b5213bae0e9e1414f964553f3e639fc6f
milesreng <miles.eng@duke.edu>
2022-02-04 14:40:22 -0500

    made SetAside much more efficient

--- APT-5/SetAside.java
+++ APT-5/SetAside.java
@@ -2,28 +2,13 @@ import java.util.*;
 
 public class SetAside {
     public String common(String[] list) {
-        HashMap<String, Integer> words = new HashMap<String, Integer>();
+        Set<String> commonWords = new TreeSet<String>(Arrays.asList(list[0].split(" ")));
+
         for (String phrase : list) {
-            String[] splitPhrase = phrase.split(" ");
-            for (String word : splitPhrase) {
-                words.putIfAbsent(word, 0);
-                words.put(word, words.get(word) + 1);
-            }
+            Set<String> phraseWords = new TreeSet<String>(Arrays.asList(phrase.split(" ")));
+            commonWords.retainAll(phraseWords);
         }
 
-        ArrayList<String> commonWords = new ArrayList<String>();
-        for (String key : words.keySet()) {
-            if (words.get(key) == list.length) {
-                commonWords.add(key);
-            }
-        }
-
-        String[] w = new String[commonWords.size()];
-        for (int i = 0; i < commonWords.size(); i++) {
-            w[i] = commonWords.get(i);
-        }
-
-        Arrays.sort(w);
-        return String.join(" ", w);
+        return String.join(" ", commonWords);
     }
 }
