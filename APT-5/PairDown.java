ccb05f7010c036ae1d0d8193f6a4bb763a37d756
milesreng <miles.eng@duke.edu>
2022-02-04 14:29:52 -0500

    made PairDown more efficient

--- APT-5/PairDown.java
+++ APT-5/PairDown.java
@@ -1,20 +1,14 @@
-import java.util.*;
-
 public class PairDown {
     public int[] fold(int[] list) {
-        ArrayList<Integer> combine = new ArrayList<Integer>();
+        int[] newList = new int[(list.length + 1) / 2];
         for (int i = 0; i < list.length; i += 2) {
             if (i == list.length - 1) {
-                combine.add(list[i]);
+                newList[i / 2] = list[i];
             } else {
-                combine.add(list[i] + list[i + 1]);
+                newList[i / 2] = list[i] + list[i + 1];
             }
         }
-        int[] comb = new int[combine.size()];
 
-        for (int i = 0; i < comb.length; i++) {
-            comb[i] = combine.get(i);
-        }
-        return comb;
+        return newList;
     }
 }
