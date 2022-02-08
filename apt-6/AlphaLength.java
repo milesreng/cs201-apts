b916e03c244e0a27449d71d3be860a9c81bdd508
milesreng <miles.eng@duke.edu>
2022-02-08 12:42:12 -0500

    AlphaLength passes 6/10 test cases

---
+++ apt-6/AlphaLength.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class AlphaLength {
+    public ListNode create(String[] words) {
+        Arrays.sort(words);
+        ListNode lastNode = new ListNode(words[words.length - 1].length());
+        ListNode currNode = new ListNode(words[0].length());
+        ArrayList<String> unique = new ArrayList<>();
+        unique.add(words[words.length - 1]);
+
+        for (int i = words.length - 2; i >= 0; i--) {
+            if (!unique.contains(words[i])) {
+                currNode = new ListNode(words[i].length());
+                currNode.next = lastNode;
+                lastNode = currNode;
+            }
+        }
+        return currNode;
+    }
+}
