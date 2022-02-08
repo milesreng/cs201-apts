97542fb67ac5b3b6813289eb39ae1e7e6b94f6f5
milesreng <miles.eng@duke.edu>
2022-02-08 12:54:57 -0500

    RemoveMin.java passes 5/7 test cases

---
+++ apt-6/RemoveMin.java
@@ -0,0 +1,24 @@
+public class RemoveMin {
+    public ListNode remove(ListNode list) {
+        int minVal = list.info;
+        ListNode currNode = list;
+        ListNode lastNode = list;
+        ListNode preMin = list;
+
+        while (currNode.next != null) {
+            lastNode = currNode;
+            currNode = currNode.next;
+            if (currNode.info < minVal) {
+                preMin = lastNode;
+                minVal = currNode.info;
+            }
+        }
+
+        if (minVal == list.info) {
+            return list.next;
+        } else {
+            preMin.next = currNode.next;
+            return list;
+        }
+    }
+}
