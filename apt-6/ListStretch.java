b5eb6cc4d7ce7a58f7c03d44f06a310e1e2672f1
milesreng <miles.eng@duke.edu>
2022-02-13 14:06:50 -0500

    completed listStretch apt

--- apt-6/ListStretch.java
+++ apt-6/ListStretch.java
@@ -1,15 +1,14 @@
-import resources.ListNode;
-
 public class ListStretch {
     public ListNode stretch(ListNode list, int amount) {
-        ListNode currNode = list;
-        // infinite loop
-        while (currNode != null) {
-            for (int i = 0; i < amount; i++) {
-                currNode.next = new ListNode(currNode.info, currNode.next);
+        ListNode first = list;
+        while (list != null) {
+            for (int i = 0; i < amount - 1; i++) {
+                list.next = new ListNode(list.info, list.next);
+                list = list.next;
             }
-            currNode = currNode.next;
+            list = list.next;
         }
-        return list;
+
+        return first;
     }
 }
