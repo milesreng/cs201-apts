da1c8ca427d7ec6586c2809bae087ab65994592b
milesreng <miles.eng@duke.edu>
2022-02-18 14:47:28 -0500

    cleaned MergeLists

--- apt-6/MergeLists.java
+++ apt-6/MergeLists.java
@@ -2,13 +2,12 @@ import resources.*;
 
 public class MergeLists {
     public ListNode weave(ListNode a, ListNode b) {
-        ListNode first = a;
         ListNode list = a;
         while (b != null) {
             list.next = new ListNode(b.info, list.next);
             list = list.next.next;
             b = b.next;
         }
-        return first;
+        return a;
     }
 }
