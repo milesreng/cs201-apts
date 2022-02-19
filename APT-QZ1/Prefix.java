e3559c26248d055a5b65751145aec80f1ff68b7b
milesreng <miles.eng@duke.edu>
2022-02-19 13:11:56 -0500

    completed Prefix.java

--- APT-QZ1/Prefix.java
+++ APT-QZ1/Prefix.java
@@ -2,33 +2,34 @@ import java.util.*;
 
 public class Prefix {
     public String[] group(String[] words) {
-        Arrays.sort(words, (a, b) -> Integer.compare(a.length(), b.length()));
-        TreeSet<ArrayList<String>> prefix = new TreeSet<>();
+        Arrays.sort(words);
+        ArrayList<ArrayList<String>> prefix = new ArrayList<>();
+        
         for (String w : words) {
-            Boolean foundComm = false;
-            for (ArrayList<String> common : prefix) {
-                if (common.get(0) == w.substring(0, common.get(0).length())) {
-                    common.add(w);
-                    foundComm = true;
+            Boolean foundPref = false;
+
+            for (ArrayList<String> prefList : prefix) {
+                String first = prefList.get(0);
+
+                if (w.length() >= first.length() && first.equals(w.substring(0, first.length()))) {
+                    foundPref = true;
+                    prefList.add(w);
                 }
             }
-            if (!foundComm) {
-                ArrayList<String> newComm = new ArrayList<>();
-                newComm.add(w);
-                prefix.add(newComm);
+
+            if (!foundPref) {
+                ArrayList<String> newPref = new ArrayList<>();
+                newPref.add(w);
+                prefix.add(newPref);
             }
         }
 
-        ArrayList<String> red = new ArrayList<>();
-        for (ArrayList<String> pref : prefix) {
-            red.add(String.join(" ", pref));
+        String[] ret = new String[prefix.size()];
+        for (int i = 0; i < ret.length; i++) {
+            ret[i] = String.join(" ", prefix.get(i));
         }
 
-        String[] ret = new String[red.size()];
-        for (int i = 0; i < red.size(); i++) {
-            ret[i] = red.get(i);
-        }
-
+        Arrays.sort(ret);
         return ret;
     }
 }
