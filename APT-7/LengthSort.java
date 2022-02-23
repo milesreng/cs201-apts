bd176f781719b9ff8aaf193a0486053c588224d1
milesreng <miles.eng@duke.edu>
2022-02-23 17:18:03 -0500

    completed LengthSort.java

--- APT-7/LengthSort.java
+++ APT-7/LengthSort.java
@@ -1,5 +1,24 @@
+import java.util.*;
+
 public class LengthSort {
     public String[] rearrange(String[] values) {
-        return null;
+        int i;
+        int j;
+        String temp;
+
+        Arrays.sort(values);
+        
+        for (i = 1; i < values.length; ++i) {
+            j = i;
+            while (j > 0 && values[j].length() < values[j - 1].length()) {
+            
+                temp = values[j];
+                values[j] = values[j - 1];
+                values[j - 1] = temp;
+                --j;
+            }
+        }
+
+        return values;
     }
 }
