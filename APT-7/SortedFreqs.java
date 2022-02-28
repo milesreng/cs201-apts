b1df28bb06bd4a57afeec549c30489bb28d326f3
milesreng <miles.eng@duke.edu>
2022-02-28 16:12:52 -0500

    completed SortedFreqs apt

--- APT-7/SortedFreqs.java
+++ APT-7/SortedFreqs.java
@@ -1,5 +1,19 @@
+import java.util.*;
+
 public class SortedFreqs {
     public int[] freqs(String[] data) {
-        return null;
+        Map<String, Integer> map = new TreeMap<>();
+        Arrays.sort(data);
+        for (String d : data) {
+            map.putIfAbsent(d, 0);
+            map.put(d, map.get(d) + 1);
+        }
+        Integer[] convert = map.values().toArray(new Integer[0]);
+        int[] counts = new int[convert.length];
+        for(int i = 0; i < counts.length;i++) {
+            counts[i] = convert[i];
+        }
+
+        return counts;
     }
 }
