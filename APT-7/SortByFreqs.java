726405427f51fb8e6ef2c56ca1570c5df714548e
milesreng <miles.eng@duke.edu>
2022-03-01 12:36:22 -0500

    completed SortByFreqs apt

--- APT-7/SortByFreqs.java
+++ APT-7/SortByFreqs.java
@@ -1,5 +1,45 @@
+import java.util.*;
+
 public class SortByFreqs {
     public String[] sort(String[] data) {
-        return null;
+        ArrayList<Word> words = new ArrayList<>();
+        ArrayList<String> dataList = new ArrayList<>(Arrays.asList(data));
+        Set<String> unique = new HashSet<>(Arrays.asList(data));
+
+        for (String word : unique) {
+            Word w = new Word(word, Collections.frequency(dataList, word));
+            words.add(w);
+        }
+
+        Comparator<Word> alph = Comparator.comparing(Word::getWord);
+        Comparator<Word> frequencies = Comparator.comparing(Word::getFreq).reversed().thenComparing(alph);
+        Collections.sort(words, frequencies);
+
+        String[] ret = new String[words.size()];
+        int i = 0;
+        for (Word w : words) {
+            ret[i] = w.getWord();
+            i++;
+        }
+
+        return ret;
+    }
+
+    private class Word {
+        private String word;
+        private int freq;
+
+        public Word(String word, int freq) {
+            this.word = word;
+            this.freq = freq;
+        }
+
+        public int getFreq() {
+            return this.freq;
+        }
+
+        public String getWord() {
+            return this.word;
+        }
     }
 }
