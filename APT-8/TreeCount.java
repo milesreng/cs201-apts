fa1aa857441271504350169c9a2c4a7a9775e39a
milesreng <miles.eng@duke.edu>
2022-03-16 20:25:46 -0400

    completed TreeCount apt

--- APT-8/TreeCount.java
+++ APT-8/TreeCount.java
@@ -1,8 +1,8 @@
-import resources.TreeNode;
-
 public class TreeCount {
     public int count(TreeNode tree) {
-        // replace with working code
-        return 0;
+        if (tree == null) {
+            return 0;
+        }
+        return 1 + count(tree.left) + count(tree.right);
     }
 }
