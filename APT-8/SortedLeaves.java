e8d7fc25693a80c0818983c6b5397d93f2e7cf45
milesreng <miles.eng@duke.edu>
2022-03-16 21:30:16 -0400

    completed SortedLeaves apt

--- APT-8/SortedLeaves.java
+++ APT-8/SortedLeaves.java
@@ -1,8 +1,33 @@
-import resources.TreeNode;
+import java.util.*;
 
 public class SortedLeaves {
     public String[] values(TreeNode tree) {
-        // replace with working code
-        return null;
+        List<Integer> vals = getLeaf(tree);
+        String[] letters = new String[vals.size()];
+        int i = 0;
+        for (Integer val : vals) {
+            int valInt = val.intValue();
+            char ch = (char)valInt;
+            letters[i] = String.valueOf(ch);
+            i++;
+        }
+        Arrays.sort(letters);
+        return letters;
+    }
+
+    public List<Integer> getLeaf(TreeNode tree) {
+        if (tree == null) {
+            return new ArrayList<>();
+        }
+        if (tree.left == null && tree.right == null) {
+            List<Integer> ret = new ArrayList<>();
+            ret.add(tree.info);
+            return ret;
+        }
+
+        List<Integer> ret = new ArrayList<>();
+        ret.addAll(getLeaf(tree.left));
+        ret.addAll(getLeaf(tree.right));
+        return ret;
     }
 }
