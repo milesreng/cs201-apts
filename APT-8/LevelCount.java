e5da3bb7fd70a78749d1acf8123b4d8a34a91615
milesreng <miles.eng@duke.edu>
2022-03-16 20:53:37 -0400

    completed LevelCount apt

--- APT-8/LevelCount.java
+++ APT-8/LevelCount.java
@@ -1,8 +1,14 @@
-import resources.TreeNode;
-
 public class LevelCount {
     public int count(TreeNode t, int level) {
-        
-        return 0;
+        return findLevel(t, level, 0);
+    }
+
+    public int findLevel(TreeNode t, int target, int currLevel) {
+        if (t == null) {
+            return 0;
+        } else if (currLevel == target) {
+            return 1;
+        }
+        return findLevel(t.left, target, currLevel + 1) + findLevel(t.right, target, currLevel + 1);
     }
 }
