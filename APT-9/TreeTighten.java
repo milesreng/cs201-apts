49fae41b3fa577b645288f9b7deb9e311c8922ae
milesreng <miles.eng@duke.edu>
2022-03-23 15:11:33 -0400

    TreeTighten apt passes 4/6 test cases

--- APT-9/TreeTighten.java
+++ APT-9/TreeTighten.java
@@ -1,5 +1,17 @@
 public class TreeTighten {
     public TreeNode tighten(TreeNode t) {
-        return null;
+        if (t == null) {
+            return null;
+        } else if (t.left == null && t.right == null) {
+            return t;
+        }
+
+        if (t.left == null) {
+            t = tighten(t.right);
+        } else if (t.right == null) {
+            t = tighten(t.left);
+        }
+
+        return t;
     }
 }
