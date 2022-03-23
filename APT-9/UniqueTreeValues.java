1273006a908fa60f04e8e44ee2d1d90fea7abdad
milesreng <miles.eng@duke.edu>
2022-03-23 15:01:27 -0400

    completed UniqueTreeValues apt

--- APT-9/UniqueTreeValues.java
+++ APT-9/UniqueTreeValues.java
@@ -1,5 +1,34 @@
+import java.util.*;
+
 public class UniqueTreeValues {
     public int[] unique(TreeNode root) {
-        return new int[0];
+        if (root == null) {
+            return new int[0];
+        }
+
+        int[] leftVals = unique(root.left);
+        int[] rightVals = unique(root.right);
+
+        Set<Integer> retSet = new HashSet<>();
+        retSet.add(root.info);
+
+        for (int i = 0; i < leftVals.length; i++) {
+            retSet.add(leftVals[i]);
+        }
+
+        for (int i = 0; i < rightVals.length; i++) {
+            retSet.add(rightVals[i]);
+        }
+
+        int[] ret = new int[retSet.size()];
+        int i = 0;
+        for (Integer val : retSet) {
+            ret[i] = val;
+            i++;
+        }
+
+        Arrays.sort(ret);
+
+        return ret;
     }
 }
