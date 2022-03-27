fa8bff8b09093fbec0219cc908ee5500d7345f42
milesreng <miles.eng@duke.edu>
2022-03-27 16:23:33 -0400

    completed leafCollector apt

--- APT-9/LeafCollector.java
+++ APT-9/LeafCollector.java
@@ -1,5 +1,51 @@
+import java.util.ArrayList;
+
+import java.util.*;
+
 public class LeafCollector {
     public String[] getLeaves(TreeNode tree) {
-        return null;
+        if (tree == null) {
+            return null;
+        }
+        List<String> levels = new ArrayList<>();
+        List<String> leaves;
+        while (!isLeaf(tree)) {
+            leaves = new ArrayList<>();
+            collectAndRemove(tree, leaves);
+            levels.add(String.join(" ", leaves));
+        }
+        levels.add("" + tree.info);
+        tree = null;
+
+        String[] ret = new String[levels.size()];
+        int i = 0;
+        for (String level : levels) {
+            ret[i] = level;
+            i++;
+        }
+        return ret;
+    }
+
+    private void collectAndRemove(TreeNode tree, List<String> list) {
+        if (tree.left != null && isLeaf(tree.left)) {
+            list.add("" + tree.left.info);
+            tree.left = null;
+        } else if (tree.left != null) {
+            collectAndRemove(tree.left, list);
+        }
+
+        if (tree.right != null && isLeaf(tree.right)) {
+            list.add("" + tree.right.info);
+            tree.right = null;
+        } else if (tree.right != null) {
+            collectAndRemove(tree.right, list);
+        }
+    }
+
+    private boolean isLeaf(TreeNode tree) {
+        if (tree.left == null && tree.right == null) {
+            return true;
+        }
+        return false;
     }
 }
