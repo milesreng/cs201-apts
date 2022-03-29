68a0e21626cf7d7731caece7906320ca75c5e4ad
milesreng <miles.eng@duke.edu>
2022-03-28 21:06:01 -0400

    completed allPaths apt

--- APT-9/AllPaths.java
+++ APT-9/AllPaths.java
@@ -1,5 +1,37 @@
+import java.util.*;
+
 public class AllPaths {
+    List<String> list = new ArrayList<>();
+
     public String[] paths(TreeNode t) {
-        return new String[0];
+        findPath(t, "");
+        String[] ret = new String[list.size()];
+        int i = 0;
+        for (String s : list) {
+            ret[i] = s;
+            i++;
+        }
+        Arrays.sort(ret);
+        return ret;
+    }
+
+    public void findPath(TreeNode t, String path) {
+        if (t == null) {
+            return;
+        }
+
+        if (path.isEmpty()) {
+            path = "" + t.info;
+        } else {
+            path += "->" + t.info;
+        }
+
+        if (t.left == null && t.right == null) {
+            list.add(path);
+            return;
+        }
+
+        findPath(t.left, path);
+        findPath(t.right, path);
     }
 }
--- APT-9/LeafTrails.java
+++ APT-9/LeafTrails.java
@@ -1,5 +1,36 @@
+import java.util.*;
+
 public class LeafTrails {
     public String[] trails(TreeNode tree) {
-        return null;
+        if (tree == null) {
+            return new String[0];
+        }
+        Map<String, String> paths = new TreeMap<>();
+        paths = encode(tree, "", paths);
+        
+
+        String[] allPaths = new String[paths.size()];
+        int i = 0;
+        for (String val : paths.values()) {
+            allPaths[i] = val;
+            i++;
+        }
+        return allPaths;
+    }
+
+    public Map<String, String> encode(TreeNode tree, String path, Map<String, String> allPaths) {
+        if (tree.left == null && tree.right == null) {
+            allPaths.put(String.valueOf(tree.info), path);
+        }
+
+        if (tree.left != null) {
+            encode(tree.left, path + "0", allPaths);
+        }
+
+        if (tree.right != null) {
+            encode(tree.right, path + "1", allPaths);
+        }
+
+        return allPaths;
     }
 }
